b97491d34672ecea06725f7549bf4ed105071679
Kesava Praveen <meetyourkeshav@gmail.com>
2023-01-28 19:35:38 +0530

    Added an output system in the BaseTest file to see the Git Hub Weebhook triggering from jenkins

--- src/test/java/com/rest/spotify/test/BaseTest.java
+++ src/test/java/com/rest/spotify/test/BaseTest.java
@@ -9,6 +9,9 @@ public class BaseTest {
     @BeforeMethod
     public void beforeTest(Method m)
     {
+        //Added the below statement to check the GITHUB Webhook Trigger
+        System.out.println("The Pipeline is running thorugh Jenkins Job and through Git Hub WebHook Process");
+
         System.out.println("Test Execution..." +m.getName());
         System.out.println("Thread Id...:" +Thread.currentThread().getId());
     }
